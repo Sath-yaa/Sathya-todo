126734efee35899bf51f753c9a5b573e84ddba86
Sath-yaa <sathyaa2315@gmail.com>
2025-07-30 16:04:31 +0530

    Update userController.js

--- b/controllers/userController.js
+++ b/controllers/userController.js
@@ -3,7 +3,7 @@ import User from "../models/user.js";
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).json({ message: "User already exists" });
+  if (userExists) return res.status(400).json({ message: "Email already exists" });
 
   const user = await User.create({ name, email, password });
   res.json({ _id: user._id, name: user.name, email: user.email });
